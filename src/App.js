9c6bb3bbf5d20835f170bc1a208ae39aeaab0c47
dmbappi.bd@gmail.com <dmbappi.bd@gmail.com>
2022-04-05 21:31:43 +0600

    services page done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Services from './Pages/Home/Services/Services';
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      
     <Services></Services>
       
     </div>
